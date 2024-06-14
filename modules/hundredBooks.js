f56409a2a6f574c3194f33f62997e4e3cc59dd77
Taylor Wagstaff <taylorwagstaff@gmail.com>
2024-06-15 00:06:15 +1200

    blue

--- modules/hundredBooks.js
+++ modules/hundredBooks.js
@@ -1,5 +1,9 @@
 import { booksData } from './bookData.js'
 
+function getRandomMinHeight(min, max) {
+  return Math.floor(Math.random() * (max - min + 1)) + min
+}
+
 function renderBooks(books) {
   const container = document.getElementById('books-container')
   const header = document.getElementById('header-number')
@@ -13,6 +17,10 @@ function renderBooks(books) {
     const bookInfo = document.createElement('div')
     bookInfo.className = 'book-info'
 
+    // Generate a unique random min-height for each book-info
+    const randomMinHeight = getRandomMinHeight(70, 150)
+    bookInfo.style.minWidth = `${randomMinHeight}px`
+
     const titleInfo = document.createElement('div')
     titleInfo.className = 'title-info'
     titleInfo.style.writingMode = 'vertical-rl'
@@ -21,11 +29,11 @@ function renderBooks(books) {
 
     const titleLeft = document.createElement('div')
     titleLeft.className = 'title-left'
-    titleLeft.innerHTML = `<p>${book.id}</p><p class="book-title">${book.title}</p>`
+    titleLeft.innerHTML = `<p>${book.id}</p><p class="book-title">${book.title}</p><p style="color: blue">+</p>`
 
     const titleRight = document.createElement('div')
     titleRight.className = 'title-right'
-    titleRight.innerHTML = `<p>${book.detailsButton.text}</p>`
+    // titleRight.innerHTML = `<p>${book.detailsButton.text}</p>`
     titleRight.style.color = 'blue'
     titleRight.style.fontSize = '16px'
     titleRight.style.marginTop = '16px'
@@ -53,19 +61,31 @@ function renderBooks(books) {
         revealText.style.display === ''
       ) {
         titleInfo.style.alignItems = 'normal'
-
-        // if clicked
-        revealText.style.display = 'block'
         titleInfo.style.writingMode = 'horizontal-tb'
+
+        // if clicked now book open...
+        revealText.style.display = 'block'
+        titleLeft.innerHTML = `<p>${book.id}</p><p class="book-title">${book.title}</p><p>-</p>`
+        bookInfo.style.minWidth = `500px`
+
+        bookInfo.style.display = 'block' // Turn off display: flex;
+        bookInfo.style.justifyContent = 'initial' // Turn off justify-content: center;
 
         titleRight.innerHTML = `<p></p>`
       } else {
         // back to book view after clicked
         revealText.style.display = 'none'
         titleInfo.style.writingMode = 'vertical-rl'
-        titleRight.innerHTML = `<p>${book.detailsButton.text}</p>`
-        titleRight.style.color = 'blue'
+        // titleRight.innerHTML = `<p>seen</p>`
+        // titleRight.style.color = 'blue'
         titleInfo.style.alignItems = 'center'
+
+        // return back to normal
+        bookInfo.style.minWidth = `${randomMinHeight}px`
+        bookInfo.style.display = 'flex' // Turn off display: flex;
+        bookInfo.style.justifyContent = 'center' // Turn off justify-content: center;
+
+        titleLeft.innerHTML = `<p>${book.id}</p><p class="book-title">${book.title}</p><p style="color: white">+</p>`
       }
     })
 
@@ -87,4 +107,21 @@ function renderBooks(books) {
 
 document.addEventListener('DOMContentLoaded', () => {
   renderBooks(booksData.books)
+  const informationButton = document.getElementById('information-button')
+  const informationText = document.getElementById('information-text')
+  const additionalText =
+    ' to help anyone who is learning art. Most of these books I wish I read earlier. Some I only encountered when I was ready for them. Others I didnt understand at first and reread them later. '
+
+  informationButton.addEventListener('click', () => {
+    if (informationText) {
+      if (informationText.innerText.includes(additionalText)) {
+        informationText.innerText = informationText.innerText.replace(
+          additionalText,
+          ''
+        )
+      } else {
+        informationText.innerText += additionalText
+      }
+    }
+  })
 })
